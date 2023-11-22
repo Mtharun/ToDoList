002f7a3b7172bb9c659c885a7a895efeccc847b0
Mtharun <87010282+Mtharun@users.noreply.github.com>
2023-11-22 11:28:13 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require('daisyui')],
+};
+
